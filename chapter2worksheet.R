dbf030c1af9ba2b60ad092df97454a544a9f82d2
Jared Wheet <jared.wheet.py10@statefarm.com>
2021-07-01 09:37:18 -0700

    chapter 1 work

---
+++ chapter2worksheet.R
@@ -0,0 +1,28 @@
+spahn <- read_csv("data/spahn.csv")
+spahn %>% 
+  mutate(FIP = (13 * HR + 3 * BB - 2 * SO) / IP) -> spahn
+
+spahn %>%
+  arrange(FIP) %>%
+  select(Year, Age, W, L, ERA, FIP) %>%
+  head()
+
+spahn %>% filter(Tm == "BSN" | Tm == "MLN") -> spahn1
+
+spahn1 <- spahn1 %>%
+  mutate(Tm = factor(Tm, levels = c("BSN", "MLN")))
+
+spahn1 %>%
+  group_by(Tm) %>%
+  summarize(mean_W.L = mean(W.L, na_rm = TRUE),
+            mean_ERA = mean(ERA),
+            mean_WHIP = mean(WHIP),
+            mean_FIP = mean(FIP))
+
+NLbatting <- read_csv("data/NLbatting.csv")
+NLpitching <-read_csv("data/NLpitching.csv")
+NL <- inner_join(NLbatting, NLpitching, by = 'Tm')
+
+NLbatting %>%
+  arrange(HR) %>%
+  filter (HR > 150) -> NL_150 
